0aa3324b21042b996aa9fb891b447596363c2f00
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-07-31 21:47:33 +0200

    cool custom shapes (Arc and Triangle)

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,13 +7,46 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        
+        let modifiedStartAngle = startAngle - rotationAdjustment
+        let modifiedEndAngle = endAngle - rotationAdjustment
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStartAngle, endAngle: modifiedEndAngle, clockwise: !clockwise)
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
+        VStack {
+            Triangle()
+                .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                .frame(width: 300, height: 300)
+            
+            Arc(startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: true)
         }
     }
 }
